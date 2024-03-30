9025e59d716ced7759e48a98eca2f15f0edb9a40
stupravin@gmail.com <stupravin@gmail.com>
2024-03-30 22:43:34 +0530

    add to cart functinality added by js only

--- core/views.py
+++ core/views.py
@@ -193,6 +193,9 @@ def add_to_cart(request):
         "title": request.GET["title"],
         "qty": request.GET["qty"],
         "price": request.GET["price"],
+        "image": request.GET["image"],
+        "pid": request.GET["pid"],
+        "price": request.GET["price"],
     }
 
     if "cart_data_obj" in request.session:
--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -129,33 +129,44 @@ $(document).ready(function () {
 
 // Add to cart functionality
 
-$("#add-to-cart-btn").on("click", function () {
-    let quantity = $("#product-quantity").val()
-    let product_title = $(".product-title").val()
-    let product_id = $(".product-id").val()
-    let product_price = $("#current-product-price").text()
+$(".add-to-cart-btn").on("click", function () {
+
     let this_val = $(this)
+    let index = this_val.attr("data-index")
+
+    let quantity = $(".product-quantity-" + index).val()
+    let product_title = $(".product-title-" + index).val()
+    let product_id = $(".product-id-" + index).val()
+    let product_price = $(".current-product-price-" + index).text()
+    let product_pid = $(".product-pid-" + index).val()
+    let product_image = $(".product-image-" + index).val()
+
 
     console.log("Quantity: ", quantity);
     console.log("Title: ", product_title);
     console.log("Price: ", product_price);
     console.log("ID: ", product_id);
+    console.log("PID: ", product_pid);
+    console.log("Image: ", product_image);
+    console.log("Index: ", index);
     console.log("Current Element: ", this_val);
 
     $.ajax({
-        url:"/add-to-cart",
-        data:{
-            "id":product_id,
-            "qty":quantity,
-            "title":product_title,
-            "price":product_price,
+        url: "/add-to-cart",
+        data: {
+            "id": product_id,
+            "pid": product_pid,
+            "image": product_image,
+            "qty": quantity,
+            "title": product_title,
+            "price": product_price,
         },
-        dataType:"json",
-        beforeSend: function(){
+        dataType: "json",
+        beforeSend: function () {
             console.log("Product adding to cart..");
         },
-        success: function(response){
-            this_val.html("Item added to cart..")
+        success: function (response) {
+            this_val.html("✓")
             console.log("Product added to cart..");
             $(".cart-items-count").text(response.totalcartitems)
         }
