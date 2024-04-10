0d5c49a510d143e43947b67260c3b9904017062e
stupravin@gmail.com <stupravin@gmail.com>
2024-04-10 10:44:58 +0530

    Revert "ready for deployment on railway"
    This reverts commit 21426a7dcf4741d27b18364ae6f0f91e1e297d25.

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-p#a#suj!*m&*ckt8+nmuun5^+k55f!sr$bw((us-dl3ji6$$%y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
